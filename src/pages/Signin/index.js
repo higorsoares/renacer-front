af5d47bec258c05bd760be1eb72058c572b3055a
Igor Soares <igorsoaress771@gmail.com>
2020-08-11 22:44:51 -0300

    Logo

--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import logo from '../../assets/logo1.jpg'
 
 export default function SignUp() {
     return (
         <>
+            <img src={ logo } alt="Igreija renascer"/>
             <form>
                 <input type="email" placeholder="Seu login"/>
                 <input type="password" placeholder="Sua senha"/>
--- src/pages/_layouts/auth/styles.js
+++ src/pages/_layouts/auth/styles.js
@@ -16,9 +16,24 @@ const Content =  styled.div`
     width: 100%;
     max-width : 315px;
     text-align : center;
-
+   
     form {
-        
+        display: flex;
+        flex-direction: column;
+        margin-top:30px;
+    }
+    input {
+        background: rgba(0,0,0,0.8);
+        border : 0;
+        border-radius: 4px;
+        height: 44px;
+        padding: 0 15px;
+        color: #fff;
+        margin : 0 0 10px;
+
+        &::placeholder {
+            color: rgba(255 , 255 , 255 , 0.7)
+        }
     }
 
 
